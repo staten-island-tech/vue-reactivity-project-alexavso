945c8b4260844620e1251180287545a6ce320d03
alexavso <alexasolanou@gmail.com>
2026-02-26 22:33:38 -0500

    .

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,47 +1,13 @@
-<template>
-  <h1> Bakery </h1>
-  
-
-
-
-</template>
-
-
 <script setup>
-const products = [
-  {
-    name: 'Lemon Cake',
-    price: 25.00,
-  },
-  {
-    name: 'Strawberry Shortcake',
-    price: 35.00,
-  },
-  {
-    name: 'Vanilla Cake',
-    price: 29.50,
-  },
-  {
-    name: 'Chocolate Cake',
-    price: 29.50,
-  },
-  {
-    name: 'Mango Jelly',
-    price: 7.50,
-  },
-  {
-    name: 'Vanilla Jelly',
-    price: 7.50,
-  },
-  {
-    name: 'Strawberry Jelly'
-  }
-]
-
 </script>
 
+<template>
+  <div>
+    <h1> Bakery </h1>
+    <nav>
+      <router-link to="/products">Products</router-link>
+    </nav>
+    <router-view />
+  </div>
 
-<style scoped>
-
-
-</style>
+</template>
--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Products from './views/products.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/products',
+      name: 'Products',
+    }
+  ],
 })
 
 export default router
