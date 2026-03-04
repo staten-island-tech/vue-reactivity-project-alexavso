3f82371d4959c0e65c5fc1eefc006fd8b648f55d
alexavso <alexasolanou@gmail.com>
2026-03-04 12:34:20 -0500

    .

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import Home from './views/Home.vue'
+import Home from '@/views/Home.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      // path: '/',
-      // name: 'Home',
-      // component: Home,
-    }
+      path:'/home',
+      name:'home',
+      component: Home,
+    },
   
   ],
 })
